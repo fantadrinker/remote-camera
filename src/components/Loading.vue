356fbfbe3c2cc23eb5b7930162b91b14c2883065
fantadrinker <yizhi.tsui@gmail.com>
2023-05-22 11:49:47 -0700

    UI stubs 2

---
+++ src/components/Loading.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="loading">
+        <div class="spinner"></div>
+        <h3>Loading...</h3>
+    </div>
+</template>
+
+<style scoped>
+.loading {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -2,6 +2,9 @@
 import { useAuth0 } from '@auth0/auth0-vue';
 import { ref, Ref, onMounted } from 'vue';
 import { getRecordings } from '../api';
+import Loading from './Loading.vue';
+
+
 interface RecordingObject {
     Key: string;
     LastModified: Date;
@@ -10,15 +13,18 @@ interface RecordingObject {
 
 const recordings: Ref<Array<RecordingObject>> = ref([]);
 
+const loading = ref(false);
+
 const { getAccessTokenSilently, user } = useAuth0();
 
 onMounted(async () => {
+    loading.value = true
     recordings.value = await getRecordings(
         await getAccessTokenSilently(),
         user.value.sub || "default"
     );
+    loading.value = false
 });
-
 
 </script>
 
@@ -26,7 +32,8 @@ onMounted(async () => {
     <div>
         <h3>Viewing</h3>
         <div class="recordings">
-            <table>
+            <loading v-if="loading" />
+            <table v-else>
                 <thead>
                     <tr>
                         <th>Date</th>
@@ -46,3 +53,11 @@ onMounted(async () => {
         </div>
     </div>
 </template>
+
+<style scoped>
+    .recordings {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
